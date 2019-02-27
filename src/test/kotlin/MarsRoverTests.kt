500c4ef6b456e2b435705917e59660bce3638a80
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-02-27 17:03:20 +0100

    Add a rover landing at given coordinates

--- src/test/kotlin/MarsRoverTests.kt
+++ src/test/kotlin/MarsRoverTests.kt
@@ -4,7 +4,12 @@ import org.junit.Test
 class MarsRoverTests {
 
     @Test
-    fun `example`() {
-        assertEquals(1, 1)
+    fun `lands with the given position`() {
+        val rover = Rover(Coordinates(2, 3))
+        assertEquals(Coordinates(2, 3), rover.coordinates)
     }
 }
+
+class Rover(val coordinates: Coordinates)
+
+data class Coordinates(private val horizontal: Int, private val vertical: Int)
