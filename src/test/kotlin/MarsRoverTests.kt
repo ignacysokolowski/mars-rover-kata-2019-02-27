f833283d8908eca170116543630efe9cc37bf040
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-02-27 17:24:50 +0100

    Add a factory method for direction north

--- src/test/kotlin/MarsRoverTests.kt
+++ src/test/kotlin/MarsRoverTests.kt
@@ -5,14 +5,18 @@ class MarsRoverTests {
 
     @Test
     fun `lands with the given position`() {
-        val rover = Rover(Direction("N"), Coordinates(2, 3))
-        assertEquals(Direction("N"), rover.direction)
+        val rover = Rover(Direction.north(), Coordinates(2, 3))
+        assertEquals(Direction.north(), rover.direction)
         assertEquals(Coordinates(2, 3), rover.coordinates)
     }
 }
 
 class Rover(val direction: Direction, val coordinates: Coordinates)
 
-data class Direction(private val symbol: String)
+data class Direction(private val symbol: String) {
+    companion object {
+        fun north() = Direction("N")
+    }
+}
 
 data class Coordinates(private val horizontal: Int, private val vertical: Int)
