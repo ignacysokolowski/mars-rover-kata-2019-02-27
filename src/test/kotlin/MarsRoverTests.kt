5c96ac5af386a760a47e916905902fcb7691f019
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-02-27 17:09:31 +0100

    Extract a class for direction

--- src/test/kotlin/MarsRoverTests.kt
+++ src/test/kotlin/MarsRoverTests.kt
@@ -5,12 +5,14 @@ class MarsRoverTests {
 
     @Test
     fun `lands with the given position`() {
-        val rover = Rover("N", Coordinates(2, 3))
-        assertEquals("N", rover.direction)
+        val rover = Rover(Direction("N"), Coordinates(2, 3))
+        assertEquals(Direction("N"), rover.direction)
         assertEquals(Coordinates(2, 3), rover.coordinates)
     }
 }
 
-class Rover(val direction: String, val coordinates: Coordinates)
+class Rover(val direction: Direction, val coordinates: Coordinates)
+
+data class Direction(private val symbol: String)
 
 data class Coordinates(private val horizontal: Int, private val vertical: Int)
