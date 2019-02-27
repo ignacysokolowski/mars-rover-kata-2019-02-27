dbdaff7296f7f0ab1e3216fcf4fdf49031913e5c
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-02-27 17:05:44 +0100

    Add direction to the rover

--- src/test/kotlin/MarsRoverTests.kt
+++ src/test/kotlin/MarsRoverTests.kt
@@ -5,11 +5,12 @@ class MarsRoverTests {
 
     @Test
     fun `lands with the given position`() {
-        val rover = Rover(Coordinates(2, 3))
+        val rover = Rover("N", Coordinates(2, 3))
+        assertEquals("N", rover.direction)
         assertEquals(Coordinates(2, 3), rover.coordinates)
     }
 }
 
-class Rover(val coordinates: Coordinates)
+class Rover(val direction: String, val coordinates: Coordinates)
 
 data class Coordinates(private val horizontal: Int, private val vertical: Int)
