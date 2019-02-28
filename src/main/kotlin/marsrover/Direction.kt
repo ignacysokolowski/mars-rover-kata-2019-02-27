7ca87f24d9ba46284f314d288e19567edf4996be
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-02-28 10:48:20 +0100

    Extract calculating the next direction

--- src/main/kotlin/marsrover/Direction.kt
+++ src/main/kotlin/marsrover/Direction.kt
@@ -6,6 +6,7 @@ enum class Direction (val stepsEast: Int, val stepsNorth: Int) {
     SOUTH(0, -1),
     WEST(-1, 0);
 
-    fun nextToTheRight() = values().elementAtOrElse(this.ordinal + 1) {values().first()}
+    fun nextToTheRight() = nextAtIndexOrElse(this.ordinal + 1, values().first())
     fun nextToTheLeft() = values().elementAtOrElse(this.ordinal - 1) {values().last()}
+    private fun nextAtIndexOrElse(index: Int, default: Direction) = values().elementAtOrElse(index) {default}
 }
