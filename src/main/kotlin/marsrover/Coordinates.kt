9b7a661a6e3bb76f7310bdbd7af84956a016e999
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-02-28 09:40:15 +0100

    Inline moving coordinates vertically

--- src/main/kotlin/marsrover/Coordinates.kt
+++ src/main/kotlin/marsrover/Coordinates.kt
@@ -1,9 +1,5 @@
 package marsrover
 
 data class Coordinates(private val horizontal: Int, private val vertical: Int) {
-    fun movedIn(direction: Direction): Coordinates {
-        return movedVerticallyBy(direction.stepsNorth)
-    }
-
-    private fun movedVerticallyBy(points: Int) = copy(vertical = vertical + points)
+    fun movedIn(direction: Direction) = copy(vertical = vertical + direction.stepsNorth)
 }
