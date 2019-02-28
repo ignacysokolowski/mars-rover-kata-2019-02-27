a8a32c7230cba3fb78dfa297604ba00eb0e67c2f
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-02-28 09:49:08 +0100

    Use +1 for direction steps

--- src/main/kotlin/marsrover/Direction.kt
+++ src/main/kotlin/marsrover/Direction.kt
@@ -1,8 +1,8 @@
 package marsrover
 
 enum class Direction (val stepsEast: Int, val stepsNorth: Int) {
-    NORTH(0, 1),
+    NORTH(0, +1),
     SOUTH(0, -1),
-    EAST(1, 0),
+    EAST(+1, 0),
     WEST(-1, 0),
 }
