3fc30df730607cdfbd4c8fe1e3b32cfff4f99823
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-02-28 14:03:04 +0100

    Add missing blank line

--- src/main/kotlin/marsrover/Coordinates.kt
+++ src/main/kotlin/marsrover/Coordinates.kt
@@ -8,6 +8,7 @@ data class Coordinates(private val horizontal: Int, private val vertical: Int) {
         if (horizontal < 0)
             throw IllegalArgumentException()
     }
+
     fun movedIn(direction: Direction) = Coordinates(
         horizontal + direction.stepsEast,
         vertical + direction.stepsNorth
