15d3da4dcd1f984206cd00fa3bfd797ddc754ccd
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-02-28 10:42:08 +0100

    Generalize getting next direction to the right

--- src/main/kotlin/marsrover/Direction.kt
+++ src/main/kotlin/marsrover/Direction.kt
@@ -2,16 +2,9 @@ package marsrover
 
 enum class Direction (val stepsEast: Int, val stepsNorth: Int) {
     NORTH(0, +1),
-    SOUTH(0, -1),
     EAST(+1, 0),
+    SOUTH(0, -1),
     WEST(-1, 0);
 
-    fun nextToTheRight(): Direction {
-        return when (this) {
-            Direction.NORTH -> Direction.EAST
-            Direction.EAST -> Direction.SOUTH
-            Direction.SOUTH -> Direction.WEST
-            else -> Direction.NORTH
-        }
-    }
+    fun nextToTheRight() = values().elementAtOrElse(this.ordinal + 1) {values().first()}
 }
