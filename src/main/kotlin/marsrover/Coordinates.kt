7394a2329519997fd77078300e8259feee1c35f6
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-02-28 09:50:07 +0100

    Split a long line

--- src/main/kotlin/marsrover/Coordinates.kt
+++ src/main/kotlin/marsrover/Coordinates.kt
@@ -1,5 +1,8 @@
 package marsrover
 
 data class Coordinates(private val horizontal: Int, private val vertical: Int) {
-    fun movedIn(direction: Direction) = Coordinates(horizontal + direction.stepsEast, vertical + direction.stepsNorth)
+    fun movedIn(direction: Direction) = Coordinates(
+        horizontal + direction.stepsEast,
+        vertical + direction.stepsNorth
+    )
 }
