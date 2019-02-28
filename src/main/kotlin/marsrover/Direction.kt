b8d50bf9bcf5f757a45c51c0386faf006272684f
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-02-28 10:50:43 +0100

    Remove duplication

--- src/main/kotlin/marsrover/Direction.kt
+++ src/main/kotlin/marsrover/Direction.kt
@@ -6,7 +6,7 @@ enum class Direction (val stepsEast: Int, val stepsNorth: Int) {
     SOUTH(0, -1),
     WEST(-1, 0);
 
-    fun nextToTheRight() = nextAtIndexOrElse(this.ordinal + 1, values().first())
-    fun nextToTheLeft() = nextAtIndexOrElse(this.ordinal - 1, values().last())
-    private fun nextAtIndexOrElse(index: Int, default: Direction) = values().elementAtOrElse(index) {default}
+    fun nextToTheRight() = nextAtIndexOrElse(+1, values().first())
+    fun nextToTheLeft() = nextAtIndexOrElse(-1, values().last())
+    private fun nextAtIndexOrElse(index: Int, default: Direction) = values().elementAtOrElse(this.ordinal + index) {default}
 }
