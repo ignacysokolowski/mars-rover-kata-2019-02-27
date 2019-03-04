1deabf09eb64e9b70c697ea5d97c1f981ea89e1f
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-03-04 16:30:37 +0100

    Make choosing next or opposite direction read more fluently

--- src/main/kotlin/marsrover/Direction.kt
+++ src/main/kotlin/marsrover/Direction.kt
@@ -6,15 +6,12 @@ enum class Direction (val stepsEast: Int, val stepsNorth: Int) {
     SOUTH(0, -1),
     WEST(-1, 0);
 
-    fun nextToTheRight() = nextAtIndexOrElse(+1) {values().first()}
-    fun nextToTheLeft() = nextAtIndexOrElse(-1) {values().last()}
-    fun opposite(): Direction = nextAtIndexOrElse(+2) {nextAtIndex(-2)}
+    fun nextToTheRight() = nextAtIndex(+1) ?: first()
+    fun nextToTheLeft() = nextAtIndex(-1) ?: last()
+    fun opposite() = nextAtIndex(+2) ?: nextAtIndex(-2)!!
 
-    private fun nextAtIndex(index: Int): Direction {
-        return values().elementAt(this.ordinal + index)
-    }
-
-    private fun nextAtIndexOrElse(index: Int, default: (Int) -> Direction): Direction {
-        return values().elementAtOrElse(this.ordinal + index, default)
-    }
+    private fun nextAtIndex(index: Int) = values().elementAtOrNull(this.ordinal + index)
+    private fun first() = values().first()
+    private fun last() = values().last()
 }
+
