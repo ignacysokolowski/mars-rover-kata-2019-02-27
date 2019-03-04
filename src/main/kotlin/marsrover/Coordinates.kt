71b73c7d5cf52be4f498c29feb39c1c6da171530
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-03-04 10:11:01 +0100

    Move protection from negative coordinates to the surface

--- src/main/kotlin/marsrover/Coordinates.kt
+++ src/main/kotlin/marsrover/Coordinates.kt
@@ -1,20 +1,11 @@
 package marsrover
 
-import java.lang.IllegalArgumentException
-
 data class Coordinates(private val horizontal: Int, private val vertical: Int) {
 
-    init {
-        if (horizontal < 0 || vertical < 0)
-            throw IllegalArgumentException()
-    }
-
     fun movedIn(direction: Direction) = Coordinates(
-        zeroIfNegative(horizontal + direction.stepsEast),
-        zeroIfNegative(vertical + direction.stepsNorth)
+        horizontal + direction.stepsEast,
+        vertical + direction.stepsNorth
     )
-
-    private fun zeroIfNegative(coordinate: Int) = if (coordinate < 0) 0 else coordinate
 
     fun fartherThan(other: Coordinates) = vertical > other.vertical || horizontal > other.horizontal
 }
--- src/main/kotlin/marsrover/Surface.kt
+++ src/main/kotlin/marsrover/Surface.kt
@@ -2,10 +2,14 @@ package marsrover
 
 class Surface private constructor(private val northEastCoordinates: Coordinates) {
 
+    private val southWestCoordinates = Coordinates(0, 0)
+
     companion object {
         fun ofSize(size: Int) = Surface(Coordinates(size, size))
     }
 
-    fun contains(coordinates: Coordinates) = !coordinates.fartherThan(this.northEastCoordinates)
+    fun contains(coordinates: Coordinates): Boolean {
+        return !coordinates.fartherThan(this.northEastCoordinates) && !this.southWestCoordinates.fartherThan(coordinates)
+    }
 
 }
