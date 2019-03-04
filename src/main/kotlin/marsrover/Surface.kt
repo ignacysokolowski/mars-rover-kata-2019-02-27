cc9552963e94b09bf45acc743603f69426f9d351
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-03-04 10:02:53 +0100

    Make Surface's constructor private

--- src/main/kotlin/marsrover/Surface.kt
+++ src/main/kotlin/marsrover/Surface.kt
@@ -1,6 +1,6 @@
 package marsrover
 
-class Surface(private val coordinates: Coordinates) {
+class Surface private constructor(private val coordinates: Coordinates) {
 
     companion object {
         fun ofSize(size: Int) = Surface(Coordinates(size, size))
