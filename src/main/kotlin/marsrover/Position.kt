07e4c1107cf40d5943e028940f0793c3194c0d97
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-03-04 15:07:52 +0100

    Remove duplication from Position methods

--- src/main/kotlin/marsrover/Position.kt
+++ src/main/kotlin/marsrover/Position.kt
@@ -1,8 +1,11 @@
 package marsrover
 
 data class Position(private val direction: Direction, internal val coordinates: Coordinates) {
-    fun movedForward() = copy(coordinates = coordinates.movedIn(direction))
-    fun movedBackward() = copy(coordinates = coordinates.movedIn(direction.opposite()))
-    fun turnedRight() = copy(direction = direction.nextToTheRight())
-    fun turnedLeft() = copy(direction = direction.nextToTheLeft())
+    fun movedForward() = withCoordinatesMovedIn(direction)
+    fun movedBackward() = withCoordinatesMovedIn(direction.opposite())
+    fun turnedRight() = facingDirection { nextToTheRight() }
+    fun turnedLeft() = facingDirection { nextToTheLeft() }
+
+    private fun withCoordinatesMovedIn(direction: Direction) = copy(coordinates = coordinates.movedIn(direction))
+    private fun facingDirection(rotated: Direction.() -> Direction) = copy(direction = direction.rotated())
 }
