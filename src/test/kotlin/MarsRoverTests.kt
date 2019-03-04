d8ff02dcf00e5a79a41c628262ca04a9759d31e4
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-03-04 12:15:45 +0100

    Put @Test on the same line with test method signatures

--- src/test/kotlin/MarsRoverTests.kt
+++ src/test/kotlin/MarsRoverTests.kt
@@ -10,139 +10,112 @@ class MarsRoverTests {
     @get:Rule
     val thrown = ExpectedException.none()!!
 
-    @Test
-    fun `lands with the given position`() {
+    @Test fun `lands with the given position`() {
         val rover = Rover.landingOn(Surface.ofSize(5), Position(Direction.NORTH, Coordinates(2, 3)))
         assertEquals(Position(Direction.NORTH, Coordinates(2, 3)), rover.position)
     }
 
-    @Test
-    fun `can not land with negative horizontal coordinate`() {
+    @Test fun `can not land with negative horizontal coordinate`() {
         assertCanNotLand(Surface.ofSize(5), Coordinates(-1, 3))
     }
 
-    @Test
-    fun `can not land with negative vertical coordinate`() {
+    @Test fun `can not land with negative vertical coordinate`() {
         assertCanNotLand(Surface.ofSize(5), Coordinates(2, -1))
     }
 
-    @Test
-    fun `can not land outside of the surface to the east`() {
+    @Test fun `can not land outside of the surface to the east`() {
         assertCanNotLand(Surface.ofSize(5), Coordinates(6, 3))
     }
 
-    @Test
-    fun `can not land outside of the surface to the north`() {
+    @Test fun `can not land outside of the surface to the north`() {
         assertCanNotLand(Surface.ofSize(5), Coordinates(2, 6))
     }
 
-    @Test
-    fun `moves forward north`() {
+    @Test fun `moves forward north`() {
         assertMovesForward(Direction.NORTH, Coordinates(2, 3), Coordinates(2, 4))
     }
 
-    @Test
-    fun `moves forward south`() {
+    @Test fun `moves forward south`() {
         assertMovesForward(Direction.SOUTH, Coordinates(2, 3), Coordinates(2, 2))
     }
 
-    @Test
-    fun `moves forward east`() {
+    @Test fun `moves forward east`() {
         assertMovesForward(Direction.EAST, Coordinates(2, 3), Coordinates(3, 3))
     }
 
-    @Test
-    fun `moves forward west`() {
+    @Test fun `moves forward west`() {
         assertMovesForward(Direction.WEST, Coordinates(2, 3), Coordinates(1, 3))
     }
 
-    @Test
-    fun `can not move forward north outside of the surface`() {
+    @Test fun `can not move forward north outside of the surface`() {
         assertMovesForward(Direction.NORTH, Coordinates(2, 5), Coordinates(2, 5))
     }
 
-    @Test
-    fun `can not move forward east outside of the surface`() {
+    @Test fun `can not move forward east outside of the surface`() {
         assertMovesForward(Direction.EAST, Coordinates(5, 2), Coordinates(5, 2))
     }
 
-    @Test
-    fun `can not move forward south from position zero`() {
+    @Test fun `can not move forward south from position zero`() {
         assertMovesForward(Direction.SOUTH, Coordinates(2, 0), Coordinates(2, 0))
     }
 
-    @Test
-    fun `can not move forward west from position zero`() {
+    @Test fun `can not move forward west from position zero`() {
         assertMovesForward(Direction.WEST, Coordinates(0, 2), Coordinates(0, 2))
     }
 
-    @Test
-    fun `moves backward when facing north`() {
+    @Test fun `moves backward when facing north`() {
         assertMovesBackward(Direction.NORTH, Coordinates(2, 3), Coordinates(2, 2))
     }
 
-    @Test
-    fun `moves backward when facing south`() {
+    @Test fun `moves backward when facing south`() {
         assertMovesBackward(Direction.SOUTH, Coordinates(2, 3), Coordinates(2, 4))
     }
 
-    @Test
-    fun `moves backward when facing east`() {
+    @Test fun `moves backward when facing east`() {
         assertMovesBackward(Direction.EAST, Coordinates(2, 3), Coordinates(1, 3))
     }
 
-    @Test
-    fun `moves backward when facing west`() {
+    @Test fun `moves backward when facing west`() {
         assertMovesBackward(Direction.WEST, Coordinates(2, 3), Coordinates(3, 3))
     }
 
-    @Test
-    fun `can not move backward outside of the surface when facing south`() {
+    @Test fun `can not move backward outside of the surface when facing south`() {
         assertMovesBackward(Direction.SOUTH, Coordinates(2, 5), Coordinates(2, 5))
     }
 
-    @Test
-    fun `can not move backward outside of the surface when facing west`() {
+    @Test fun `can not move backward outside of the surface when facing west`() {
         assertMovesBackward(Direction.WEST, Coordinates(5, 2), Coordinates(5, 2))
     }
 
-    @Test
-    fun `turns right from north to east`() {
+    @Test fun `turns right from north to east`() {
         assertTurnsRight(Direction.NORTH, Direction.EAST)
     }
 
-    @Test
-    fun `turns right from east to south`() {
+    @Test fun `turns right from east to south`() {
         assertTurnsRight(Direction.EAST, Direction.SOUTH)
     }
 
-    @Test
-    fun `turns right from south to west`() {
+    @Test fun `turns right from south to west`() {
         assertTurnsRight(Direction.SOUTH, Direction.WEST)
     }
 
-    @Test
-    fun `turns right from west to north`() {
+    @Test fun `turns right from west to north`() {
         assertTurnsRight(Direction.WEST, Direction.NORTH)
     }
 
-    @Test
-    fun `turns left from north to west`() {
+    @Test fun `turns left from north to west`() {
         assertTurnsLeft(Direction.NORTH, Direction.WEST)
     }
 
-    @Test
-    fun `turns left from west to south`() {
+    @Test fun `turns left from west to south`() {
         assertTurnsLeft(Direction.WEST, Direction.SOUTH)
     }
 
-    @Test
-    fun `turns left from south to east`() {
+    @Test fun `turns left from south to east`() {
         assertTurnsLeft(Direction.SOUTH, Direction.EAST)
     }
 
-    @Test
-    fun `turns left from east to north`() {
+    @Test fun `turns left from east to north`() {
         assertTurnsLeft(Direction.EAST, Direction.NORTH)
     }
 
@@ -180,23 +153,19 @@ class MarsRoverTests {
 
 class CoordinatesTests {
 
-    @Test
-    fun `not farther than another`() {
+    @Test fun `not farther than another`() {
         assertFalse(Coordinates(3, 5).fartherThan(Coordinates(4, 6)))
     }
 
-    @Test
-    fun `as far as the other`() {
+    @Test fun `as far as the other`() {
         assertFalse(Coordinates(3, 3).fartherThan(Coordinates(3, 3)))
     }
 
-    @Test
-    fun `farther north than another`() {
+    @Test fun `farther north than another`() {
         assertTrue(Coordinates(3, 3).fartherThan(Coordinates(3, 2)))
     }
 
-    @Test
-    fun `farther east than another`() {
+    @Test fun `farther east than another`() {
         assertTrue(Coordinates(3, 3).fartherThan(Coordinates(2, 3)))
     }
 }
